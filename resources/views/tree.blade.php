58918e8923c35e3b3a8c62db542f9b13f2ad3fb9
brobert <robert.blonski+git@gmail.com>
2016-02-25 18:07:38 +0100

    initial commit

---
+++ resources/views/tree.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Tree widget</title>
+        <script type="text/javascript" src="/js/jquery-2.2.1.min.js"></script>
+        <style type="text/css" href="/styles/bootstrap.min.css"></style>
+    </head>
+    <body>
+
+    </body>
+</html>
